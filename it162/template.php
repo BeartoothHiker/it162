2a68dc0f3e6803d9fcbefd04a05d73df2bbbf98e
Jeremy R <72467083+BeartoothHiker@users.noreply.github.com>
2022-01-23 18:49:38 -0800

    Update 'content' PHP files for header/footer

--- it162/template.php
+++ it162/template.php
@@ -1,13 +1,13 @@
 <?php 
 include 'includes/header.php';
 ?>
-        
+
 <!-- START LEFT COL -->
 <section>
- <h2 class="pageID">Flowchart</h2>
+ <h2 class="pageID">TEMPLATE</h2>
+ <p>Use this as a model of how to add new pages.</p>
 </section>
 <!-- END LEFT COL -->
-
 <!-- START RIGHT COL -->
 <aside>
 </aside>
