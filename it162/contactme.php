614c3558c7fa8f3461b77a322b7a5c966323e998
Jeremy R <72467083+BeartoothHiker@users.noreply.github.com>
2022-03-21 12:07:36 -0700

    Form Assignment IT162

--- it162/contactme.php
+++ it162/contactme.php
@@ -5,6 +5,25 @@ include 'includes/header.php';
 <!-- START LEFT COL -->
 <section>
  <h2 class="pageID">Contact Jeremy</h2>
+  <?php
+        /*
+         * Below are 2 different forms to be re-used       
+         * 
+         * Only use one at a time, comment out the other!       
+         *
+         */
+
+        include 'includes/contact_include.php'; #site keys & code here
+    
+        $toAddress  = "mostpuzzled@outlook.com";  //place your/your client's email address here
+        $toName     = "CodeWithJeremy"; //place your client's name here
+        $website    = "CodeWithJeremy IT162 Projects";  //place NAME of your client's website,
+            // $website is used for client to identify webiste, and or page
+
+        //echo loadContact('simple.php');#demonstrates a simple contact form
+        echo loadContact('multiple.php');#demonstrates multiple form elements
+  ?>
+
 </section>
 <!-- END LEFT COL -->
 
